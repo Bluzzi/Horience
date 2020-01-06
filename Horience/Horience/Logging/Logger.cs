cc353d75ceab246019a35cb31c898ba70aaa0282
Seyz <36236750+Seyz123@users.noreply.github.com>
2020-01-05 17:52:03 -0800

    Typo in Logger class (VS has no auto-save ¯\_(ツ)_/¯)

--- Horience/Horience/Logging/Logger.cs
+++ Horience/Horience/Logging/Logger.cs
@@ -20,13 +20,13 @@ namespace Horience.Logging
         public void Error(string message)
         {
             DateTime dateTime = DateTime.Now;
-            Console.WriteLine(LoggerColors.Red + "[" + dateTime.Hour + ":" + dateTime.Minute + ":" + dateTime.Second + "] [" + GetPrefix() + "/INFO] " + message);
+            Console.WriteLine(LoggerColors.Red + "[" + dateTime.Hour + ":" + dateTime.Minute + ":" + dateTime.Second + "] [" + GetPrefix() + "/ERROR] " + message);
         }
 
         public void Debug(string message)
         {
             DateTime dateTime = DateTime.Now;
-            Console.WriteLine(LoggerColors.Purple + "[" + dateTime.Hour + ":" + dateTime.Minute + ":" + dateTime.Second + "] [" + GetPrefix() + "/INFO] " + message);
+            Console.WriteLine(LoggerColors.Purple + "[" + dateTime.Hour + ":" + dateTime.Minute + ":" + dateTime.Second + "] [" + GetPrefix() + "/DEBUG] " + message);
         }
 
         public string GetPrefix()
