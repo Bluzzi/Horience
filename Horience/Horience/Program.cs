4f9a0ddc77f996aced310fd69cb3fce216a92e7f
Seyz <36236750+Seyz123@users.noreply.github.com>
2020-01-05 17:06:29 -0800

    Base
    Ya pas grand chose à part le projet VS

---
+++ Horience/Horience/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Horience
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program(args);
+        }
+
+        public Program(string[] args)
+        {
+            Console.WriteLine("Starting Horience...");
+        }
+    }
+}
