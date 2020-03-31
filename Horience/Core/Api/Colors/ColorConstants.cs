3a5bfac105f4ff536ddd2fa871cccab459206714
Bluzzi <bluzzigame@gmail.com>
2020-03-31 22:52:24 +0200

    Create ColorConstants class for project colors

---
+++ Horience/Core/Api/Colors/ColorConstants.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows.Media;
+
+namespace Horience.Core.Api.Colors
+{
+    class ColorConstants
+    {
+        public static Color RED = Color.FromRgb(116, 20, 20);
+        public static Color GREEN = Color.FromRgb(0, 185, 67);
+    }
+}
