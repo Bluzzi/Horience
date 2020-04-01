9ed817697737f8697fdf3b63ae180aa61414125c
Bluzzi <bluzzigame@gmail.com>
2020-04-01 23:32:57 +0200

    ColorConstants: add DARK_GRAY color

--- Horience/Core/Api/Colors/ColorConstants.cs
+++ Horience/Core/Api/Colors/ColorConstants.cs
@@ -6,7 +6,8 @@ namespace Horience.Core.Api.Colors
     {
         public static Color RED = (Color)ColorConverter.ConvertFromString("#FF5555");
         public static Color GREEN = (Color)ColorConverter.ConvertFromString("#55FF55");
-        public static Color GRAY = (Color)ColorConverter.ConvertFromString("#FFAAAAAA");
+        public static Color GRAY = (Color)ColorConverter.ConvertFromString("#2C2F33");
+        public static Color DARK_GRAY = (Color)ColorConverter.ConvertFromString("#23272A");
         public static Color WHITE = (Color)ColorConverter.ConvertFromString("#FFFFFFFF");
     }
 }
