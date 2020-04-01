f6cb8f5ad1908f38e8077e738dd26af12cee22e2
Bluzzi <bluzzigame@gmail.com>
2020-04-01 13:01:00 +0200

    ColorConstants: update project colors

--- Horience/Core/Api/Colors/ColorConstants.cs
+++ Horience/Core/Api/Colors/ColorConstants.cs
@@ -4,7 +4,9 @@ namespace Horience.Core.Api.Colors
 {
     class ColorConstants
     {
-        public static Color RED = Color.FromRgb(116, 20, 20);
-        public static Color GREEN = Color.FromRgb(0, 185, 67);
+        public static Color RED = (Color)ColorConverter.ConvertFromString("#FF5555");
+        public static Color GREEN = (Color)ColorConverter.ConvertFromString("#55FF55");
+        public static Color GRAY = (Color)ColorConverter.ConvertFromString("#FFAAAAAA");
+        public static Color WHITE = (Color)ColorConverter.ConvertFromString("#FFFFFFFF");
     }
 }
