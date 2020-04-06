51759805a52053b1bc4850e1b5e9927c28ec1e24
Bluzzi <bluzzigame@gmail.com>
2020-04-06 19:05:16 +0200

    Panel: other test...

--- Horience/Core/Panel.xaml.cs
+++ Horience/Core/Panel.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace Horience.Core
@@ -9,15 +11,23 @@ namespace Horience.Core
         {
             InitializeComponent();
 
-            for(int tgm = 0; tgm < 10; tgm++)
+            for(int tgm = 0; tgm < 25; tgm++)
             {
                 Button category = new Button
                 {
+                    //Name = tgm.ToString(),
                     Content = "Woah " + tgm.ToString()
                 };
 
+                category.Name = "hello" + tgm.ToString();
+                category.Click += OnClickButton;
                 Category.Children.Add(category);
             }
+        }
+
+        private void OnClickButton(object sender, RoutedEventArgs e)
+        {
+            ((Button)sender).Content = new Random().Next(1, 10).ToString();
         }
 
         private void ChangeSize(object sender, SizeChangedEventArgs e)
