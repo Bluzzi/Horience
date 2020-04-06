4432dfe8520732699a69164becd804e3d3ca08d8
Bluzzi <bluzzigame@gmail.com>
2020-04-06 17:09:59 +0200

    App: create Start() method for beta support

--- Horience/App.xaml.cs
+++ Horience/App.xaml.cs
@@ -1,8 +1,22 @@
-﻿using System.Windows;
+﻿using Horience.Core;
+using System.Windows;
 
 namespace Horience
 {
     public partial class App : Application
     {
+        private void Start(object sender, StartupEventArgs e)
+        {
+            if (Info.IS_BETA)
+            {
+                new Main((int)Core.Main.MODES.ALL);
+            }
+            else
+            {
+                Injector Window = new Injector();
+
+                Window.Show();
+            }
+        }
     }
 }
