bc57115ed8220ad75ab6f843fdadbcd9798f6219
Bluzzi <bluzzigame@gmail.com>
2020-04-06 19:04:19 +0200

    Panel: add ChangeSize() method (cool style when the window is not in full screen)

--- Horience/Core/Panel.xaml.cs
+++ Horience/Core/Panel.xaml.cs
@@ -19,5 +19,21 @@ namespace Horience.Core
                 Category.Children.Add(category);
             }
         }
+
+        private void ChangeSize(object sender, SizeChangedEventArgs e)
+        {
+            if (e.PreviousSize == new Size(0, 0)) return;
+
+            Window Sender = (Window)sender;
+
+            if (Sender.Height == e.PreviousSize.Height && Sender.Width == e.PreviousSize.Width)
+            {
+                Sender.BorderThickness = new Thickness(0);
+            } 
+            else
+            {
+                Sender.BorderThickness = new Thickness(2);
+            }
+        }
     }
 }
