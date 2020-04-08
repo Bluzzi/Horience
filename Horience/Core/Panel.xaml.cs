dbfb48aa3e9a612be27a587681e80261d525c572
Bluzzi <bluzzigame@gmail.com>
2020-04-08 14:29:12 +0200

    Panel: divers tests...

--- Horience/Core/Panel.xaml.cs
+++ Horience/Core/Panel.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Horience.Core.UI;
+using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
@@ -9,28 +11,38 @@ namespace Horience.Core
     {
         private Chat ChatUI;
 
+        private Dictionary<string, string[]> Categories = new Dictionary<string, string[]>();
+
         public Panel()
         {
             InitializeComponent();
 
-            for(int tgm = 0; tgm < 25; tgm++)
             ChatUI = new Chat(Chat);
+
+            Categories.Add("World", new string[] { "test", "other test" });
+            Categories.Add("Combat", new string[] { "test", "other test" });
+            Categories.Add("Macro", new string[] { "test", "other test" });
+            Categories.Add("Utility", new string[] { "test", "other test" });
+            Categories.Add("Render", new string[] { "test", "other test" });
+            Categories.Add("Blatant", new string[] { "test", "other test" });
+
+            foreach (var CategoryInfo in Categories)
             {
                 Button category = new Button
                 {
-                    //Name = tgm.ToString(),
-                    Content = "Woah " + tgm.ToString()
+                    Name = CategoryInfo.Key.ToString(),
+                    Content = CategoryInfo.Key.ToString()
                 };
 
-                category.Name = "hello" + tgm.ToString();
                 category.Click += OnClickButton;
+
                 Category.Children.Add(category);
             }
         }
 
         private void OnClickButton(object sender, RoutedEventArgs e)
         {
-            ((Button)sender).Content = new Random().Next(1, 10).ToString();
+            Debug.WriteLine(String.Join(" ", Categories[((Button)sender).Content.ToString()]));
         }
 
         // UI Getters :
