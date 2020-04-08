045bce33c4fe67fa6553a41d44b2f20cb3a4db55
Bluzzi <bluzzigame@gmail.com>
2020-04-08 14:26:59 +0200

    Create Chat class in new UI folder

---
+++ Horience/Core/UI/Chat.cs
@@ -0,0 +1,30 @@
+﻿using Horience.Core.Api.Colors;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Horience.Core.UI
+{
+    class Chat
+    {
+        private StackPanel ChatList;
+
+        public Chat(StackPanel ChatList)
+        {
+            this.ChatList = ChatList;
+        }
+
+        public void AddMessage(string SenderName, string Message)
+        {
+            Label MessageLabel = new Label()
+            {
+                Content = SenderName + " : " + Message,
+                Margin = new Thickness(25, 0, 0, 0),
+                FontSize = 20,
+                Foreground = new SolidColorBrush(ColorConstants.WHITE)
+            };
+
+            ChatList.Children.Add(MessageLabel);
+        }
+    }
+}
